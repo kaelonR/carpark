77712e6850cadf0d52c8d3e69f34384019d1a515
J141 <j.dekoning141@gmail.com>
2023-08-27 19:28:19 +0200

    Add Car and CarStatus domain models

---
+++ Carpark.Domain/Car.cs
@@ -0,0 +1,21 @@
+﻿namespace Carpark.Domain;
+
+public class Car
+{
+    public Car(string licensePlate, string colour, int constructionYear, CarStatus carStatus)
+    {
+        LicensePlate = licensePlate;
+        Colour = colour;
+        ConstructionYear = constructionYear;
+        CarStatus = carStatus;
+    }
+
+    public string LicensePlate { get; set; }
+    public string Colour { get; set; }
+    public int ConstructionYear { get; set; }
+    public CarStatus CarStatus { get; set; }
+    
+    public string LentTo { get; set; } = string.Empty;
+
+    public string Comments { get; set; } = string.Empty;
+}
---
+++ Carpark.Domain/CarStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace Carpark.Domain;
+
+public enum CarStatus
+{
+    OnOrder = 1,
+    Available = 2,
+    LentOut = 3,
+    UnderRepairs = 4,
+    Sold = 5
+}
